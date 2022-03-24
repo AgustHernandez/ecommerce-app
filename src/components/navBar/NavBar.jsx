1d460094027d085c7daa8fe82ce9f96bde61123c
AgustHernandez <hagustinadaniela@gmail.com>
2022-03-24 19:15:26 -0300

    Modificate ItemList

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import './styleNavBar.css';
+
 import Cart from '../Cart/Cart';
 import CartWidget from '../cartWidget/CartWidget';
 import HeroSection from '../heroSection/HeroSection';
 import MsjBienvenida from '../msjBienvenida/MsjBienvenida';
-
-import './styleNavBar.css';
+import { NavLink } from 'react-router-dom';
+import React from 'react'
 
 const NavBar = () => {
 
@@ -24,6 +24,9 @@ const NavBar = () => {
                 </div>
                 <NavLink to="/cart" element={<Cart/>} className="carrito" > <CartWidget/> </NavLink>
             </nav>
+            {
+                
+            }
             <HeroSection MsjBienvenida={MsjBienvenida}/>
         </header>
     );
