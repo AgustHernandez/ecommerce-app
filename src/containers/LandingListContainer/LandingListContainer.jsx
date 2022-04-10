1c19e52d12660c109dacc542e7bda621ca705f93
AgustHernandez <hagustinadaniela@gmail.com>
2022-04-10 11:16:55 -0300

    Agrego boton VER MAS

--- src/containers/LandingListContainer/LandingListContainer.jsx
+++ src/containers/LandingListContainer/LandingListContainer.jsx
@@ -3,6 +3,7 @@ import './styleLandingListContainer.css';
 import { collection, getDocs, getFirestore, limit, query } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 
+import { Button } from '@mui/material/Button';
 import HeroSection from '../../components/heroSection/HeroSection';
 import ItemList from '../../components/ItemList/ItemList';
 import MsjBienvenida from '../../components/msjBienvenida/MsjBienvenida';
@@ -31,7 +32,13 @@ function LandingListContainer() {
         <h2 className='tituloProd'>PRODUCTOS</h2>
         {loading ? <h2 className='loading'>Cargando...</h2>
           :
-          <ItemList productos={productos} />}
+          <div>
+            <ItemList productos={productos} />
+            <div>
+              <Button variant="contained" >VER MAS</Button>
+            </div>
+          </div>
+        }
       </div>
     </>
   )
