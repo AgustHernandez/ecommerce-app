c925193528d01868302fb58ad3ad55aaf596e2b9
AgustHernandez <hagustinadaniela@gmail.com>
2022-04-10 11:10:17 -0300

    modificacion

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import "./LoadingSpinner.css";
+
+import React from "react";
 
 export default function LoadingSpinner() {
   return (
     <div className="spinner-container">
-      <div className="loading-spinner">
-      </div>
+      <div className="loading-spinner"/>
     </div>
   );
 }
