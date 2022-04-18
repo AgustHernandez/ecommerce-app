a9f1adf4e4bf298d5753e1f2e8f62fcc53791c51
AgustHernandez <hagustinadaniela@gmail.com>
2022-04-18 15:59:18 -0300

    Agrega Footer

--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,8 @@
+import { Box, CircularProgress, Container } from '@mui/material';
 import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 
-import { Box } from '@mui/material';
 import ItemList from '../../components/ItemList/ItemList';
-import LoadingSpinner from './../../components/LoadingSpinner/LoadingSpinner';
 import { useParams } from 'react-router-dom';
 
 function ItemListContainer() {
@@ -31,15 +30,15 @@ function ItemListContainer() {
 
   return (
     <>
-      <Box maxWidth="xl">
+      <Container maxWidth="xl" sx={{ heigth: 'auto', mb: 20 }} >
         <h2 className='tituloProd'>PRODUCTOS</h2>
         {loading ? 
-          <div>
-            <LoadingSpinner/>
-          </div>
+          <Box maxWidth="xl" sx={{ m: '5rem', display: 'flex', justifyContent: 'center' }}>
+            <CircularProgress color="primary" sx={{color: '#b5838d'}}  />
+          </Box>
           :
           <ItemList productos={productos} />}
-      </Box>
+      </Container>
     </>
   )
 }
